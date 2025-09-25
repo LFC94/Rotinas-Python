2d9768f932a42c021381661a5827abec5dbeeb32
Lucas Felipe Costa <lucasfelipecost4@gmail.com>
2025-09-25 10:45:30 -0300

    Adds investment feature and dependencies
    Adds the 'yfinance' and 'deep-translator' packages to the project's dependencies.
    
    Integrates an investment feature into the main menu, allowing users to access investment-related functionalities.

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 
-from src import censo, g2, moodle, relatorio, usu, youtube
+from src import censo, g2, invest, moodle, relatorio, usu, youtube
 from src.util.uteis import menuTelas
 
 load_dotenv()
@@ -9,11 +9,12 @@ load_dotenv()
 def menu():
     MENU = {
         '1': {'title': 'Robos G2', 'function': g2.inicio},
-        '5': {'title': 'Relatorio', 'function': relatorio.inicio},
         '2': {'title': 'Moodle', 'function': moodle.inicio},
         '3': {'title': 'Censo', 'function': censo.inicio},
-        '6': {'title': 'Santa Ursula', 'function': usu.inicio},
         '4': {'title': 'Youtube', 'function': youtube.inicio},
+        '5': {'title': 'Relatorio', 'function': relatorio.inicio},
+        '6': {'title': 'Santa Ursula', 'function': usu.inicio},
+        '7': {'title': 'Investimento', 'function': invest.inicio},
     }
 
     menuTelas(MENU)
